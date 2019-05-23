4f880f2a99fc419406df6e4deea674a8d8b63c6a
League Student <league@iMac.attlocal.net>
2019-05-22 18:26:17 -0700

    Riddler IP
    :question: :
    
    GET IT?
    question mark, RIDDLER!!!????
    (I'm not actually confused)

--- src/Remarkable.java
+++ src/Remarkable.java
@@ -7,8 +7,8 @@ public class Remarkable {
 		String name = JOptionPane.showInputDialog("What's Your Name?? (Sorry, Ima Stalker...) PLEASE capatalize the beggining letter ");
 		
 		if ( name.equals("Ameer")) {
-			JOptionPane.showMessageDialog(null, "Happy Birthday Ameer!(#stalkerAGAIN) You're the best bro on the planet! BTW, your freckles are soooooooooooooooo frickin'");
-			
+			JOptionPane.showMessageDialog(null, "Aeeeeeeeeey, Meeru!(#stalkerAGAIN) You're the best bro on the planet! BTW, your freckles are soooooooooooooooo frickin'");
+		}
 			if ( name.equals("Colin")) {
 				JOptionPane.showMessageDialog(null, "Wasssssup Colin!(#stalkerAGAIN) I'm sooo glad that you're my bros bff. BTW, your freckles are soooooooooooooooo frickin'");
 				
@@ -20,6 +20,8 @@ public class Remarkable {
 						
 						if ( name.equals("Ameer")) {
 							JOptionPane.showMessageDialog(null, "Happy Birthday Ameer!(#stalkerAGAIN) You're the best bro on the planet! BTW, your freckles are soooooooooooooooo frickin'");
+						if ( name.equals("Ms. Jackie")) {
+							JOptionPane.showMessageDialog(null, "YO! Ms. Jackie! (#stalkerAGAIN) Your an awesome 'coder.' YOU'RE AWESOME!  You're the best teacher on the planet! '");
 							
 			
 			
---
+++ src/Riddler.java
@@ -0,0 +1,8 @@
+
+public class Riddler {
+
+	
+	public static void main(String[] args) {
+		
+	}
+}
